8aeaa988a1a2116df89ed76039ec13f7dcb2b78d
ajala ahmad <ahmadajala98@gmail.com>
2021-11-01 18:49:23 +0800

    multiple task

--- src/chapterTwo/Multiples.java
+++ src/chapterTwo/Multiples.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Multiples {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number1;
-        int number2;
+        int firstNumber;
+        int secondNumber;
         System.out.println("Enter first number");
-        number1 = input.nextInt();
+        firstNumber = input.nextInt();
         System.out.println("Enter second Number");
-        number2 = input.nextInt();
-        if(number2 % number1 ==0)
+        secondNumber = input.nextInt();
+        if(secondNumber % firstNumber ==0)
             System.out.println("first number is a multiple of the second number");
-        if(number2 % number1 != 0)
+        if(secondNumber % firstNumber != 0)
             System.out.println("first number is not a multiple of the second number");
 
     }
