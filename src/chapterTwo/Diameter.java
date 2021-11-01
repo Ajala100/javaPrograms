9fb3bc88927dd82b0197652df536e8ccc83bf9d9
ajala ahmad <ahmadajala98@gmail.com>
2021-11-01 18:44:03 +0800

    Diameter task

--- src/chapterTwo/Diameter.java
+++ src/chapterTwo/Diameter.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Diameter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int radius;
+        int circleRadius;
         System.out.println("Enter radius");
-        radius = input.nextInt();
-        double circumference= 2 * Math.PI* radius;
-        int diameter= 2 * radius;
-        double area = Math.PI * (radius * radius) ;
+        circleRadius = input.nextInt();
+        double circumference= 2 * Math.PI* circleRadius;
+        int diameter= 2 * circleRadius;
+        double area = Math.PI * (circleRadius * circleRadius) ;
         System.out.println("circumference is " +circumference);
         System.out.println("diameter is " +diameter);
         System.out.println("area is " +area);
