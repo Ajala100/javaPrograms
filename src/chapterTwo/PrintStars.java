03b86b675e53ff371f91413e468d5ca6c7c0bdc6
ajala ahmad <ahmadajala98@gmail.com>
2021-11-01 18:38:55 +0800

    print stars task

---
+++ src/chapterTwo/PrintStars.java
@@ -0,0 +1,81 @@
+package chapterTwo;
+
+public class PrintStars {
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*********");
+
+
+        // printing second shape
+        System.out.println("   ***   ");
+        System.out.println(" *     * ");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println(" *     * ");
+        System.out.println("   ***   ");
+
+        //printing third shape
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println("*****");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+        //printing the fourth shape
+        System.out.println("    *    ");
+        System.out.println("   * *   ");
+        System.out.println("  *   *  ");
+        System.out.println(" *     * ");
+        System.out.println("*       *");
+        System.out.println(" *     * ");
+        System.out.println("  *   *  ");
+        System.out.println("   * *   ");
+        System.out.println("    *    ");
+
+        //printing next task
+        System.out.printf("*%n**%n***%n****%n*****%n");
+
+        //printing next task
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("**");
+
+        //printing next task
+        System.out.print("*");
+        System.out.print("***");
+        System.out.print("*****");
+        System.out.print("****");
+        System.out.println("**");
+
+        //printing next task
+        System.out.print("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.print("****");
+        System.out.println("**");
+
+        //printing next task
+        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+
+        //printing next task
+
+
+
+
+    }
+}
