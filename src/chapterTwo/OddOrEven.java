f6c6c6f4994e253a48649aa097b81efc3d0662db
ajala ahmad <ahmadajala98@gmail.com>
2021-11-02 16:41:09 +0800

    odd or even task

--- src/chapterTwo/OddOrEven.java
+++ src/chapterTwo/OddOrEven.java
@@ -2,14 +2,13 @@ package chapterTwo;
 import java.util.Scanner;
 public class OddOrEven {
     public static void main(String[] args) {
-        int number;
+        int value;
         Scanner input = new Scanner(System.in);
         System.out.println("enter number");
-        number = input.nextInt();
-        if(number % 2 ==0)
+        value = input.nextInt();
+        if(value % 2 ==0)
             System.out.println("number is an even number");
-        if (number % 2 != 0)
+        if (value % 2 != 0)
             System.out.println("number is an odd number");
-
     }
 }
