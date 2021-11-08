68c3332d08262729e4a748398b57dd5d05394c77
ajala ahmad <ahmadajala98@gmail.com>
2021-11-08 21:33:39 +0800

    Table of squares task

---
+++ src/chapterTwo/TableOfSquaresAndCubes.java
@@ -0,0 +1,19 @@
+package chapterTwo;
+
+public class TableOfSquaresAndCubes {
+    public static void main(String[] args) {
+        System.out.println("The squares and cubes of numbers from zero to ten are: ");
+        System.out.printf("%-10s%-10s%-10s%n", "number", "square", "cube");
+        System.out.printf("%-10d%-10d%-10d%n", 0, 0*0, 0*0*0);
+        System.out.printf("%-10d%-10d%-10d%n", 1, 1*1, 1*1*1);
+        System.out.printf("%-10d%-10d%-10d%n", 2, 2*2, 2*2*2);
+        System.out.printf("%-10d%-10d%-10d%n", 3, 3*3, 3*3*3);
+        System.out.printf("%-10d%-10d%-10d%n", 4, 4*4, 4*4*4);
+        System.out.printf("%-10d%-10d%-10d%n", 5, 5*5, 5*5*5);
+        System.out.printf("%-10d%-10d%-10d%n", 6, 6*6, 6*6*6);
+        System.out.printf("%-10d%-10d%-10d%n", 7, 7*7, 7*7*7);
+        System.out.printf("%-10d%-10d%-10d%n", 8, 8*8, 8*8*8);
+        System.out.printf("%-10d%-10d%-10d%n", 9, 9*9, 9*9*9);
+        System.out.printf("%-10d%-10d%-10d%n", 10, 10*10, 10*10*10);
+    }
+}
